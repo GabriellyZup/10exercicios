8dc0a41a3e5de44f978d8b59fc0a39212a011a05
Gabrielly Boareto <gabrielly.boareto@zup.com.br>
2024-12-06 23:08:41 -0300

    soma de numeros ex1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        int soma = 0;
+        for (int contador = 1; contador <= numero; contador++) {
+            soma += contador;
         }
+        System.out.println("A soma dos números de 1 até " + numero + " é: " + soma);
     }
 }
